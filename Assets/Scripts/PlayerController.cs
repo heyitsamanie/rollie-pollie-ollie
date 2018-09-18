07123bfacd574156db5aba536361d93d4b464433
heyitsamanie <42983780+heyitsamanie@users.noreply.github.com>
2018-09-18 13:13:08 -0500

    #3: Completed Camera Contoller Tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,22 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody rb;
-  
+    public float speed;
 
-    // Use this for initialization
+    private Rigidbody rb;
+
     void Start()
     {
-
-
         rb = GetComponent<Rigidbody>();
-
-
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement);
-
-
-
+        rb.AddForce(movement * speed);
     }
 }
