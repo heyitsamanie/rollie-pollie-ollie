eb5a17e2b6c91a2ca3ae3207f7a77e14043f8644
heyitsamanie <42983780+heyitsamanie@users.noreply.github.com>
2018-09-20 13:47:28 -0500

    #7: Displaying our score and win screen!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count; 
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -29,7 +37,20 @@ public class PlayerController : MonoBehaviour
 
              {
                 other.gameObject.SetActive(false);
-             }
+                count = count + 1;
+                SetCountText ();
+
+             }   
          }
+
+        void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 13)
+        {
+            winText.text = "You Win!!!";
+        }
+
+    }
 
 }
